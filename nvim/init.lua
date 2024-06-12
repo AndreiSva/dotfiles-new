f39aabd33f9b77260f471d400016ca2e703f0e73
Andrei <andreisva2023@gmail.com>
2024-06-12 16:46:55 -0700

    update dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,28 +18,7 @@ require("lazy").setup({
 	},
 
 	{
-		'github/copilot.vim',
-		config = function()
-			vim.cmd('Copilot disable')
-		end
-	},
-
-	{
-		'andweeb/presence.nvim',
-	},
-
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "canary",
-		dependencies = {
-			{ "github/copilot.vim" }, -- or github/copilot.vim
-			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-		},
-		opts = {
-			debug = true, -- Enable debugging
-			-- See Configuration section for rest
-		},
-		-- See Commands section for default commands if you want to lazy load on them
+		"norcalli/nvim-colorizer.lua"
 	},
 
 	{
@@ -81,47 +60,6 @@ require("lazy").setup({
 		opts = {} -- this is equalent to setup({}) function
 	},
 	
-	{
-		'mfussenegger/nvim-dap',
-		config = function()
-			local dap = require('dap')
-			dap.adapters.lldb = {
-				type = 'executable',
-				command = vim.api.nvim_exec2('echo exepath("lldb-vscode")', {output = true})['output'],
-				name = 'lldb',
-			}
-			dap.configurations.cpp = {
-				{
-					name = 'Launch',
-					type = 'lldb',
-					request = 'launch',
-					program = function()
-						-- return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-						return './debugProg'
-					end,
-					cwd = '${workspaceFolder}',
-					stopOnEntry = false,
-					args = {},
-					runInTerminal = true,
-				},
-			}
-
-
-		dap.configurations.c = dap.configurations.cpp
-
-		local widgets = require('dap.ui.widgets')
-		local sidebar = widgets.sidebar(widgets.scopes)
-
-		vim.keymap.set('n', 't', dap.toggle_breakpoint)
-		vim.keymap.set('n', '<Leader>c', dap.continue)
-		vim.keymap.set('n', '<Leader>n', dap.step_over)
-		vim.keymap.set('n', '<Leader>s', dap.step_into)
-		vim.keymap.set('n', '<Leader>w', sidebar.toggle)
-		vim.keymap.set('n', '<Leader>r', dap.repl.toggle)
-		vim.keymap.set('n', '<Leader>q', dap.terminate)
-		end,
-	},
-
 	{
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
